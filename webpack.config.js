bb0909dfa1e45f479d41ed34a94c3f4f2e80282d
domster704 <domster704@mail.ru>
2024-03-29 17:31:12 +0300

    add webpack and babel build

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require("webpack");
+const path = require("path");
+const dotenv = require("dotenv").config({
+    path: path.join(__dirname, '.env')
+});
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: './src/index.jsx',
+    mode: 'development',
+    output: {
+        path: __dirname + '/build',
+        filename: 'main.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    // {
+                    //     // options: {
+                    //     //     modules: true
+                    //     // }
+                    // }
+                ]
+            }, {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": JSON.stringify(process.env)
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: __dirname + '/build',
+        },
+        compress: true,
+        port: 9000,
+        client: {
+            progress: false,
+        },
+        open: true,
+    }
+}
